59ce9cfbe1f9f8b70efa2558f511f70955ff3490
Juan Florez <juan.florez@ceiba.com.co>
2018-01-26 16:04:35 -0500

    Se crea la prueba unitaria(incompleta) de calcular tarifa.

--- parqueadero/src/main/java/co/com/ceiba/parqueadero/domain/VigilanteService.java
+++ parqueadero/src/main/java/co/com/ceiba/parqueadero/domain/VigilanteService.java
@@ -1,5 +1,7 @@
 package co.com.ceiba.parqueadero.domain;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
 import co.com.ceiba.parqueadero.model.Registro;
@@ -19,5 +21,13 @@ public class VigilanteService {
 	 public boolean crearIngreso(Registro registro) {
 		
 		return false;
+	}
+
+	public int calcularTarifa(Date fechaInicial, Date fechaFinal) {
+		// TODO Auto-generated method stub
+		float diff = (fechaFinal.getTime()-fechaInicial.getTime())/60000;	
+		
+		System.out.println(diff/60);
+		return 0;
 	}
 }
--- parqueadero/src/test/java/co/com/ceiba/parqueadero/domain/VigilanteServiceTest.java
+++ parqueadero/src/test/java/co/com/ceiba/parqueadero/domain/VigilanteServiceTest.java
@@ -1,5 +1,10 @@
 package co.com.ceiba.parqueadero.domain;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import org.junit.*;
 
 import co.com.ceiba.parqueadero.model.Moto;
@@ -33,4 +38,25 @@ public class VigilanteServiceTest {
 		Assert.assertEquals(seCreo, false);
 	}
 	
+	/**
+	 * Prueba unitaria para calcular tarifa.
+	 */
+	@Test
+	public void calcularTarifaTest(){
+		//Assert 
+	    Calendar calendarInicial = Calendar.getInstance();
+	    calendarInicial.set(2018, 0, 25, 10, 00);
+	    Calendar calendarFinal = Calendar.getInstance();
+		
+	    Date fechaInicial = calendarInicial.getTime();
+	    Date fechaFinal = calendarFinal.getTime();
+	    
+	    //Act
+	    int tarifa = vigilanteService.calcularTarifa(fechaInicial, fechaFinal);
+	    
+	    //Assert
+	    Assert.assertEquals(1, tarifa);
+	    
+		
+	}
 }
